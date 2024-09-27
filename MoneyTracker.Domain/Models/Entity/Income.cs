264d3d7c5e3d183d55b8aa7b20cbf6d96d42dc42
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-09-27 12:16:21 +0500

    Models

---
+++ MoneyTracker.Domain/Models/Entity/Income.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTracker.Domain.Models.Entity
+{
+    public class Income : BaseTransaction
+    {
+        public override decimal Calculate(decimal curranteAmount)
+        {
+            return curranteAmount + Amount;
+        }
+    }
+}
---
+++ MoneyTracker.Domain/Models/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTracker.Domain.Models.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public decimal Balance{ get; set; }
+        public List<User> Incomes { get; set; }
+        public List<User> Expenses { get; set; }
+
+    }
+}
