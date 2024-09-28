327a02acee9aa597edfc24730dab9f02c048db8b
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-09-28 14:11:00 +0500

    dashboard add

--- MoneyTracker/Controllers/HomeController.cs
+++ MoneyTracker/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using MoneyTracker.Application.Interfaces.Service;
+using MoneyTracker.Domain.Models;
+using MoneyTracker.Domain.Models.DTO;
 using MoneyTracker.Models;
 using System.Diagnostics;
 
@@ -6,16 +9,35 @@ namespace MoneyTracker.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUserService _userService;
+        private readonly IIncomeService _incomeService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUserService userService, IIncomeService incomeService)
         {
-            _logger = logger;
+            _userService = userService;
+            _incomeService = incomeService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> Dashboard(UserDTO userDTO)
+        {
+            var responseUser = await _userService.LoginAsync(userDTO);
+            
+            if(responseUser.Result==null) { 
+                //return View("/Views/Home/Index.cshtml");
+                return View(new ResponseModel<DashboardModel>(responseUser.Error));
+            }
+            //var responseIncomes = await _incomeService;
+            DashboardModel responseDashModel = new DashboardModel()
+            {
+                User = responseUser.Result,
+
+            };
+
+            return View(new ResponseModel<DashboardModel>(responseDashModel));
         }
 
         public IActionResult Privacy()
