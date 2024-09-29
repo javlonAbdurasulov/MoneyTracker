1f08544568bf8870996585e1063274ec486f4a7d
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-09-29 14:25:12 +0500

    ss

--- MoneyTracker.Infrastructure/Filter/FilterService.cs
+++ MoneyTracker.Infrastructure/Filter/FilterService.cs
@@ -9,37 +9,37 @@ namespace MoneyTracker.Infrastructure.Filter
 {
     public class FilterService<T> where T : class,IDate,ICategory,IAmount
     {
-        public static IQueryable<T> FilterByDateRange(IQueryable<T> queryable, DateTime startDate, DateTime endDate)
+        public static IQueryable<T> FilterByDate(IQueryable<T> queryable, DateTime startDate, DateTime endDate)
         {
             return queryable.Where(t => t.Date >= startDate && t.Date <= endDate);
         }
 
-        public static IQueryable<T> FilterByType(IQueryable<T> queryable, string category)
+        public static IQueryable<T> FilterByCategory(IQueryable<T> queryable, string category)
         {
             return queryable.Where(t => t.Category == category);
         }
 
-        public static IQueryable<T> OrderByDateAsc(IQueryable<T> queryable)
+        public static IQueryable<T> OrderByDateUp(IQueryable<T> queryable)
         {
             return queryable.OrderBy(t => t.Date);
         }
 
-        public static IQueryable<T> OrderByDateDesc(IQueryable<T> queryable)
+        public static IQueryable<T> OrderByDateDown(IQueryable<T> queryable)
         {
             return queryable.OrderByDescending(t => t.Date);
         }
 
-        public static IQueryable<T> OrderByAmountAsc(IQueryable<T> queryable)
+        public static IQueryable<T> OrderByAmountUp(IQueryable<T> queryable)
         {
             return queryable.OrderBy(t => t.Amount);
         }
 
-        public static IQueryable<T> OrderByAmountDesc(IQueryable<T> queryable)
+        public static IQueryable<T> OrderByAmountDown(IQueryable<T> queryable)
         {
             return queryable.OrderByDescending(t => t.Amount);
         }
 
-        public static IQueryable<BaseTransaction> MergeAndOrderByDate(IQueryable<Income> incomes, IQueryable<Expense> expenses)
+        public static IQueryable<BaseTransaction> MargeCategory(IQueryable<Income> incomes, IQueryable<Expense> expenses)
         {
             var transactions = incomes.Cast<BaseTransaction>()
                                       .Concat(expenses.Cast<BaseTransaction>());
