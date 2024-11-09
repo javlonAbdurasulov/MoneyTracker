86dbf1348b2048eb5d858724f26179a66383c0b4
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-11-09 23:37:38 +0500

    ss

---
+++ MoneyTracker.Infrastructure/AppConfiguration/MigrationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MoneyTracker.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTracker.Infrastructure.AppConfiguration
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            using AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
--- MoneyTracker/Program.cs
+++ MoneyTracker/Program.cs
@@ -22,6 +22,10 @@ namespace MoneyTracker
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            if (app.Environment.IsDevelopment())
+            {
+                app.ApplyMigrations();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
